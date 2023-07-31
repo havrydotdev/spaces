4cad5fcace30a7dd1faa24339269cdebced804fd
Ivan <fistashkin0123@gmail.com>
2023-08-01 02:55:33 +0300

    delete getServerSideProps from NotesPage

--- app/(routes)/notes/page.tsx
+++ app/(routes)/notes/page.tsx
@@ -1,24 +1,6 @@
 "use client";
 import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next";
 
-const NotesPage = ({
-  active,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {};
-
-export const getServerSideProps: GetServerSideProps<{
-  active: string | undefined;
-}> = async (ctx: GetServerSidePropsContext) => {
-  const active = ctx.req.cookies["active_dir"];
-
-  return {
-    props: {
-      active,
-    },
-  };
-};
-
-export default NotesPage;
+const NotesPage = () => {};
